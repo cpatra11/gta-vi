9fee2668486214fa035ae77f428766ec2f252dbd
Chirantan <cpun1213@gmail.com>
2025-10-07 15:47:55 +0530

    Fix case sensitivity in NavBar import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
 import Hero from "./sections/Hero.jsx";
-import NavBar from "./sections/NavBar.jsx";
+import NavBar from "./sections/Navbar.jsx";
 import FirstVideo from "./sections/FirstVideo.jsx";
 import Jason from "./sections/Jason.jsx";
 import SecondVideo from "./sections/SecondVideo.jsx";
